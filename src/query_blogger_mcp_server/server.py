7d792788d5c18f3b75b9f4d222bbad0a6ef2d7e8
karthiks <karthiks@users.noreply.github.com>
2025-08-05 13:19:24 +0530

    Change log level to INFO from DEBUG

--- src/query_blogger_mcp_server/server.py
+++ src/query_blogger_mcp_server/server.py
@@ -14,7 +14,7 @@ from query_blogger_mcp_server.blogger_api_client import BloggerAPIClient
 from query_blogger_mcp_server.config import settings
 
 # Configure logging
-logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
 
 # --- Initialize Blogger API client using settings ---
